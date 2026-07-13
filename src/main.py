674bf893bd557c733406636ae0025737316350a3
Harshit Pawar <harshitpawar3011@gmail.com>
2026-07-13 15:07:37 +0530

    Show only online devices

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 from ping import ping_host
 
 print("===================================")
-print("      NETWORK SCANNER v1")
+print("      NETWORK SCANNER v3")
 print("      Developed by Spidey")
 print("===================================")
 
 network = input("Enter network (Example: 192.168.1): ")
 
+online_devices = 0
+
+print("\nScanning...\n")
+
 for i in range(1, 255):
     ip = f"{network}.{i}"
 
-    print(f"\nScanning {ip}...")
-
     if ping_host(ip):
-        print("✅ Device is Online")
-    else:
-        print("❌ Device is Offline")
+        print(f"✅ {ip}")
+        online_devices += 1
+
+print("\n-----------------------------------")
+print("Scan Complete")
+print(f"Devices Found: {online_devices}")
--- src/ping.py
+++ src/ping.py
@@ -3,7 +3,7 @@ import subprocess
 
 def ping_host(ip):
     result = subprocess.run(
-        ["ping", "-n", "1", ip],
+        ["ping", "-n", "1", "-w", "300", ip],
         capture_output=True,
         text=True
     )
