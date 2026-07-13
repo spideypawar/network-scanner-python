5154e8ebbf6ed46f61d88911fef0f80cb92dc68c
Harshit Pawar <harshitpawar3011@gmail.com>
2026-07-13 15:01:14 +0530

    Add network range scanning

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
-import ipaddress
 from ping import ping_host
-
-ips = [
-    "127.0.0.1",
-    "8.8.8.8",
-    "1.1.1.1",
-    "192.168.1.1"
-]
 
 print("===================================")
 print("      NETWORK SCANNER v1")
 print("      Developed by Spidey")
 print("===================================")
 
-for ip in ips:
+network = input("Enter network (Example: 192.168.1): ")
+
+for i in range(1, 255):
+    ip = f"{network}.{i}"
+
     print(f"\nScanning {ip}...")
 
     if ping_host(ip):
