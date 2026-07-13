1cc59e162d2fef7ccdfb4d4a44475b5dc32be7cf
Harshit Pawar <harshitpawar3011@gmail.com>
2026-07-13 14:58:59 +0530

    Scan multiple IP addresses

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 import ipaddress
 from ping import ping_host
 
+ips = [
+    "127.0.0.1",
+    "8.8.8.8",
+    "1.1.1.1",
+    "192.168.1.1"
+]
+
 print("===================================")
-print("     NETWORK SCANNER v1")
+print("      NETWORK SCANNER v1")
 print("      Developed by Spidey")
 print("===================================")
 
-ip = input("Enter an IP address: ")
-
-try:
-    ipaddress.ip_address(ip)
+for ip in ips:
+    print(f"\nScanning {ip}...")
 
     if ping_host(ip):
         print("✅ Device is Online")
     else:
         print("❌ Device is Offline")
-
-except ValueError:
-    print("❌ Invalid IP Address")
