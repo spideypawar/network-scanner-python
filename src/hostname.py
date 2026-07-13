06d6fa301bab27507ffd80decce0af8cf85a6386
Harshit Pawar <harshitpawar3011@gmail.com>
2026-07-13 20:44:48 +0530

    Accept full IP address as input

--- src/hostname.py
+++ src/hostname.py
@@ -1,16 +1,8 @@
 import socket
 
 
-def save_report(online_devices_info, online_devices, time_taken):
-    with open("scan_results.txt", "w") as file:
-        file.write("NETWORK SCANNER RESULTS\n")
-        file.write("=======================\n\n")
-        file.write("Online Devices\n")
-        file.write("-----------------------\n")
-
-        for ip, hostname in online_devices_info:
-            file.write(f"{ip} ({hostname})\n")
-
-        file.write("\n-----------------------\n")
-        file.write(f"Devices Found: {online_devices}\n")
-        file.write(f"Time Taken: {time_taken:.2f} seconds\n")
+def get_hostname(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return "Unknown"
--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ print("      NETWORK SCANNER v4")
 print("      Developed by Spidey")
 print("===================================")
 
-network = input("Enter network (Example: 192.168.1): ")
+network = input("Enter IP or Network (Example: 192.168.1 or 192.168.1.8): ").strip()
+
+parts = network.split(".")
+
+if len(parts) == 4:
+    network = ".".join(parts[:3])
 
 scan_network(network)
