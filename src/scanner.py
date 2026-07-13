66fb33a135e1bb8f48f591b52fce8aa7dfb8f7c9
Harshit Pawar <harshitpawar3011@gmail.com>
2026-07-13 20:07:35 +0530

    Add scan progress and timer

--- src/scanner.py
+++ src/scanner.py
@@ -1,23 +1,33 @@
 from ping import ping_host
+import time
 
 
 def scan_network(network):
     online_devices = 0
     online_ips = []
 
+    total_ips = 254
+
     print("\nScanning...\n")
+
+    start_time = time.time()
 
     for i in range(1, 255):
         ip = f"{network}.{i}"
+
+        print(f"Scanning {i}/{total_ips}: {ip}")
 
         if ping_host(ip):
             print(f"✅ {ip}")
             online_devices += 1
             online_ips.append(ip)
 
+    end_time = time.time()
+
     print("\n-----------------------------------")
     print("Scan Complete")
     print(f"Devices Found: {online_devices}")
+    print(f"Time Taken: {end_time - start_time:.2f} seconds")
 
     with open("scan_results.txt", "w") as file:
         file.write("NETWORK SCANNER RESULTS\n")
@@ -30,5 +40,6 @@ def scan_network(network):
 
         file.write("\n-----------------------\n")
         file.write(f"Devices Found: {online_devices}\n")
+        file.write(f"Time Taken: {end_time - start_time:.2f} seconds\n")
 
     print("\n💾 Results saved to scan_results.txt")
